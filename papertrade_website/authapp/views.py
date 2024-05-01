b39f19791f69372ebf9c32a1c3430338f02de836
bmajkut9 <bmajkut9@gmail.com>
2024-05-01 16:37:54 -0400

    5/1 - Built registration back end

--- papertrade_website/authapp/views.py
+++ papertrade_website/authapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,5 +9,14 @@ def login(request):
     return render(request, "login.html")
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/home/")
+        else:
+            return render(request, "signup.html", {"form": form})
+    else:
+        form = UserCreationForm()
+
     return render(request, "signup.html", {"form": form})
