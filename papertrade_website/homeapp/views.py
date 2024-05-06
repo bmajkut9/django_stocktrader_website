76030b4e1a6de7299455db7f4bdf2c24a050e190
bmajkut9 <bmajkut9@gmail.com>
2024-05-05 22:17:01 -0400

    5/5 - Built biggest winners and losers in market API getter and basic display logic
    - TODO: Build winners/losers graph, basic home UI, and news

--- papertrade_website/homeapp/views.py
+++ papertrade_website/homeapp/views.py
@@ -1,9 +1,41 @@
+import requests
 from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def home(request):
+    API_KEY = "9K40A1BBDYXSDIM8"
+    url = "https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=" + API_KEY
+    gainers = []
+    losers = []
+    
+    r = requests.get(url)
+    data = r.json()
+    gainers_data = data.get('top_gainers', [])
+    losers_data = data.get('top_losers', [])
+    
+    for gainer in gainers_data[:10]:
+        gainer_data = []
+        gainer_ticker = gainer.get("ticker", "N/A")
+        gainer_data.append(gainer_ticker)
+        gainer_change_percentage = gainer.get("change_percentage", "N/A")
+        gainer_data.append(gainer_change_percentage)
+        
+        gainers.append(gainer_data)
+        
+    for loser in losers_data [:10]:
+        loser_data = {}
+        loser_ticker = loser.get("ticker", "N/A")
+        loser_change_percentage = loser.get("change_percentage", "N/A")
+        
+        loser_data["ticker"] = loser_ticker
+        loser_data["change_percentage"] = loser_change_percentage
+                
+        losers.append(loser_data)
+    
+    
+    
     if request.user.is_authenticated:
-        return render(request, "home.html")
+        return render(request, "home.html", {"gainers": gainers, "losers": losers})
     else:
         return redirect("login")
