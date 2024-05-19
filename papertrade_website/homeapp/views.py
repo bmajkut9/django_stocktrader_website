55ac51ebea67ede6695d540e7f5636c415592590
bmajkut9 <bmajkut9@gmail.com>
2024-05-19 00:00:13 -0400

    5/18 - Built complete chart logic - TODO: Fix chart display date

--- papertrade_website/homeapp/views.py
+++ papertrade_website/homeapp/views.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from bs4 import BeautifulSoup
 from django.shortcuts import render, redirect
 from django.db.models import Sum
+from django.contrib.auth.decorators import login_required
 from investments.models import BuyStockHistory, SellStockHistory
 
 # Create your views here.
@@ -28,7 +29,48 @@ def get_date_range(user): # arg = request.user
     else:
         return None, None, None
 
+# gets each date where something happened and gives the overall ending value for that date
+def aggregate_stock_values(user):
+    data = {}
+
+    buy_data = BuyStockHistory.objects.filter(user=user).values('buy_date').annotate(total_value=Sum('stock_value_amount'))
+    sell_data = SellStockHistory.objects.filter(user=user).values('sell_date').annotate(total_value=Sum('stock_value_amount'))
+    
+    for entry in buy_data:
+        date = entry['buy_date']
+        if date in data:
+            data[date] += entry['total_value']
+        else:
+            data[date] = entry['total_value']
+        
+    for entry in sell_data:
+        date = entry['sell_date']
+        if date in data:
+            data[date] -= entry['total_value']
+        else:
+            data[date] = -entry['total_value']
+    
+    sorted_data = dict(sorted(data.items()))
+    return sorted_data 
+            
+
 def home(request):
+    chart_dates_config = {}
+    chart_dates_config["min_date"], chart_dates_config["max_date"], chart_dates_config["unit_type"] = get_date_range(request.user)
+    
+    stock_values = aggregate_stock_values(request.user)
+    dates = list(stock_values.keys())
+    formatted_dates = []
+    cumulative_values = []
+    cumulative_value = 0
+    
+    for date in dates:
+        formatted_dates.append(date.strftime('%Y-%m-%d'))
+        cumulative_value += stock_values[date]
+        cumulative_values.append(cumulative_value)
+        
+    chart_data = {"dates": formatted_dates, "cumulative_values": cumulative_values}
+    
     gainers = []
     losers = []
     mostActive = []
@@ -62,13 +104,21 @@ def home(request):
     
     print("All articles", articles)
             
-    
+    # for total investments text display
     investment_assets = BuyStockHistory.objects.filter(user = request.user).aggregate(total_investment=Sum('stock_value_amount'))['total_investment']
     if not investment_assets:
         investment_assets = 0
         
-
+    print(chart_data)
+    print(chart_dates_config)
     if request.user.is_authenticated:
-        return render(request, "home.html", {"chart_increment_type": ,"chart_dates": , "cumulative_values": ,"investment_assets": investment_assets, "gainers": gainers, "losers": losers, "mostActive": mostActive, "articles": articles})
+        return render(request, "home.html", {
+            "chart_dates_config": chart_dates_config, 
+            "chart_data": chart_data, 
+            "investment_assets": investment_assets, 
+            "gainers": gainers, 
+            "losers": losers, 
+            "mostActive": mostActive, 
+            "articles": articles})
     else:
         return redirect("login")
